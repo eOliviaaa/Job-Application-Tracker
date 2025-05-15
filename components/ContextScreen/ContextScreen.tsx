1464a74db6e8416075c9fec06735c55b8438dc9c
eOliviaaa <eoliviaaa@protonmail.com>
2025-05-15 21:55:52 +0200

    filled in some information in a table on the ContextScreen

--- components/ContextScreen/ContextScreen.tsx
+++ components/ContextScreen/ContextScreen.tsx
@@ -1,11 +1,20 @@
 'use client';
-import { Paper, Title, useMantineTheme } from "@mantine/core";
+import { Paper, useMantineTheme, Table, TableData } from "@mantine/core";
 
 export function ContextScreen() {
     const theme = useMantineTheme();
+
+    const tableData: TableData = {
+        head: ['Company', 'Response', 'Comment'],
+        body: [
+            ['test', 'test', 'test'],
+            ['test', 'test', 'test'],
+        ]  
+    }
+
     return(
         <Paper shadow="md" w={"75%"} h={"97vh"} m={"1rem"} ml={"0"} bg={theme.colors.gray[0]}>
-            
+            <Table data={tableData} />
         </Paper>
     );
 }
